1bfac0b910103addc12a0648b15d474ab8553820
Ilya Khadykin <mageofit@gmail.com>
2016-02-17 21:46:13 +0300

    Task 4: Added support of user input from the console

--- FindMinimum.java
+++ FindMinimum.java
@@ -1,6 +1,12 @@
-public class FindMinimum {
+import java.util.Scanner;
+import java.lang.System;
+import java.util.ArrayList;
 
-  public static int findMin(int ... nums) {
+public class FindMinimum {
+  
+  private ArrayList<Integer> numsList = new ArrayList<Integer>();
+
+  public static int findMin(Integer ... nums) {
       int min = nums[0];
       for(int i = 0; i < nums.length; i++) {
           if (min > nums[i]) {
@@ -9,11 +15,41 @@ public class FindMinimum {
       }
       return min;
   }
+
+  private static void testFindMin() {
+     System.out.println("\nTesting findMin method: ");
+     System.out.println("min(0) = " + findMin(0));
+     System.out.println("min(4,2) = " + findMin(4,2));
+     System.out.println("min(8,2, -1) = " + findMin(8,2,-1));
+     System.out.println("min(4364,45, 0, -55) = " + findMin(4364,45, 0, -55) + "\n");
+  }
+
+  public void requestUserInput() {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Please enter integer numbers.");
+       System.out.println("To finish your input please enter any character and press Enter:");
+       while (sc.hasNextInt()) {
+            numsList.add(sc.nextInt());
+       }
+
+       if (numsList.size() == 0) {
+         System.out.println("You didn't enter any integer numbers, try again\n");
+         requestUserInput();
+       } else {
+         Integer[] nums = new Integer[numsList.size()];
+         nums = numsList.toArray(nums);
+
+         System.out.println("min(" + java.util.Arrays.toString(nums) + ") = "
+                + findMin(nums));
+       }
+  }
   
   public static void main(String[] args) {
-      System.out.println("Finding Minimum");
-      System.out.println("min(4,2) = " + findMin(4,2));
-      System.out.println("min(8,2, -1) = " + findMin(8,2,-1));
-      System.out.println("min(4364,45, 0, -55) = " + findMin(4364,45, 0, -55));
+      System.out.println("SERACNING THE LOWEST VALUE IN A GIVEN LIST OF INTEGERS");
+      System.out.println("-------------------------------------------------------");
+
+      FindMinimum fm = new FindMinimum();
+      testFindMin();
+      fm.requestUserInput();
   }
 }
--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 ## Task 4 Find Minimum
 
 Create a class `FindMinimum` with the following methods:
+
 1. `findMin` that takes 2 integer arguments and returns the lowest
 2. `findMin` that takes 2 integer arguments and returns the lowest (using the first method)
 3. `findMin` that takes 2 integer arguments and returns the lowest (using the previous method)
+
+See the solution - [FindMinimum.java](FindMinimum.java)
